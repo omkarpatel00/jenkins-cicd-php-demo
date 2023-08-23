54961918f888619f6f406838490070abe806dc9f
Omkar Patel <60734599+omkarpatel00@users.noreply.github.com>
2023-08-23 12:53:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,45 @@
 <html>
 <head>
     <title>Sample PHP Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem 0;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 2rem;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #333;
+            margin-bottom: 1rem;
+        }
+        p {
+            color: #666;
+            line-height: 1.6;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello, From Cloud4c </h1>
-    <p>This is a sample PHP page deployed using Jenkins Pipeline.</p>
-    <p>raja</p>
+    <header>
+        <h1>Hello, From Cloud4c</h1>
+    </header>
+    <div class="container">
+        <p>This is a sample PHP page deployed using Jenkins Pipeline.</p>
+        <p>Client: John Doe</p>
+        <p>Contact: john.doe@example.com</p>
+    </div>
 </body>
 </html>
